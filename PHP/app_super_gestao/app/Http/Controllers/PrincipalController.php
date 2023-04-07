0586fe0297dd471201d0d31576602a5d16cfe604
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-04-06 21:02:53 -0300

    Update: Super Gestão - Implementando as visualizações principal, sobre-nos e contato

--- PHP/app_super_gestao/app/Http/Controllers/PrincipalController.php
+++ PHP/app_super_gestao/app/Http/Controllers/PrincipalController.php
@@ -8,6 +8,6 @@ class PrincipalController extends Controller
 {
     public function principal()
     {
-        echo 'Olá seja bem vindo ao cursos';
+        return view('site.principal');
     }
 }
--- PHP/app_super_gestao/app/Http/Controllers/SobreNosController.php
+++ PHP/app_super_gestao/app/Http/Controllers/SobreNosController.php
@@ -8,6 +8,6 @@ class SobreNosController extends Controller
 {
     public function sobre()
     {
-        echo 'Sobre-nos';
+        return view('site.sobre-nos');
     }
 }
