79a77d93522821e079732998b3b2b430f9ef92ff
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-05-02 20:28:10 -0300

    Update: Blade-@unless

--- PHP/app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ PHP/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,25 +1,22 @@
 <h3>Fonecedor</h3>
 
-
-{{ 'Texto de teste' }}
-<?= 'Texto de teste' ?>
-
-{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
-
 @php
-    /*if() {
-
-    } elseif (condition) {
-        # code...
-    } else {
-        # code...
-    }*/
+    /*
+        if() {} //enquanto executa se o retorno for true
+    */
 @endphp
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
+{{-- @unless executa se o retorno for false --}}
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+@if( !($fornecedores[0]['status'] == 'S') )
+    Fornecedor inativo
 @endif
+<br>
+@unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condição for false -->
+    Fornecedor inativo
+@endunless
+<br>
