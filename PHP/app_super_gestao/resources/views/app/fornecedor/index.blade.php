084f834facdec4cad46d354a03724db3721da52d
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-05-04 21:24:24 -0300

    Update: Blade: while

--- PHP/app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ PHP/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -9,7 +9,8 @@
 {{-- @unless executa se o retorno for false --}}
 
 @isset($fornecedores)
-    @for($i = 0; isset($fornecedores[$i]); $i++)
+    @php $i = 0 @endphp
+    @while(isset($fornecedores[$i]))
         Fornecedor: {{ $fornecedores[$i]['nome'] }}
         <br>
         Status: {{ $fornecedores[$i]['status'] }}
@@ -18,7 +19,8 @@
         <br>
         Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
         <hr>
-    @endfor
+        @php $i++ @endphp
+    @endwhile
 @endisset
 
 <br>
