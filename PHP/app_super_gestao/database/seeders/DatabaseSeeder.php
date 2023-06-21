f7937ae65ccce3bacd39279a06f539e47133e81e
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-06-21 17:20:38 -0300

    Update: Seeders #02

--- PHP/app_super_gestao/database/seeders/DatabaseSeeder.php
+++ PHP/app_super_gestao/database/seeders/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(FornecedorSeeeder::class);
+        $this->call(SiteContatoSeeder::class);
     }
 }
