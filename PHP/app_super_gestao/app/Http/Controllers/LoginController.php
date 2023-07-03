5596c72274bed112190763e8a502a1edf711dccf
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-07-03 12:39:35 -0300

    Update: Validando a existência do usuário e senha no Banco de Dados

--- PHP/app_super_gestao/app/Http/Controllers/LoginController.php
+++ PHP/app_super_gestao/app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class LoginController extends Controller
 {
@@ -28,6 +29,22 @@ class LoginController extends Controller
         // se não passar pelo validate
         $request->validate($regras, $feedback);
 
-        print_r($request->all());
+        // recuperamos os parâmetros do formulário
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
+
+        echo "Usuário: $email | Senha: $password";
+        echo '<br>';
+
+        // iniciar o model user
+        $user = new User();
+
+        $usuario = $user->where('email', $email)->where('password', $password)->get()->first();
+
+        if(isset($usuario->name)) {
+            echo 'Usuário existe';
+        } else {
+            echo 'Usuário não existe';
+        }
     }
 }
