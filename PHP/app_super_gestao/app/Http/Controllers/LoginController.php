9b46856af078a7a21eb0ba33fd53e2fcc13c87c5
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-07-03 12:19:21 -0300

    Update: Recebendo os parâmetros de usuário e senha

--- PHP/app_super_gestao/app/Http/Controllers/LoginController.php
+++ PHP/app_super_gestao/app/Http/Controllers/LoginController.php
@@ -10,8 +10,24 @@ class LoginController extends Controller
     {
         return view('site.login', ['titulo' => 'Login']);
     }
-    public function autenticar()
+
+    public function autenticar(Request $request)
     {
-        return 'Chegamos até aqui!';
+        // regras de validação
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        //mensagens de feedback de validação
+        $feedback = [
+            'usuario.email' => 'O campo usuário (e-mail) é obrigatório.',
+            'senha.required' => 'O campo senha é obrigatório.'
+        ];
+
+        // se não passar pelo validate
+        $request->validate($regras, $feedback);
+
+        print_r($request->all());
     }
 }
