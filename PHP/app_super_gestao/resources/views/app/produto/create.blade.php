74a0aa694a6ea67efde7477dd1a49b0a57c08db5
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-07-26 15:03:46 -0300

    Update: Modificando a forma de edição de registros #02

--- PHP/app_super_gestao/resources/views/app/produto/create.blade.php
+++ PHP/app_super_gestao/resources/views/app/produto/create.blade.php
@@ -5,11 +5,7 @@
 @section('conteudo')
     <div class="conteudo-pagina">
         <div class="titulo-pagina-2">
-            @if (isset($produto->id))
-                <p>Editar Produto</p>
-            @else
-                <p>Adicionar Produto</p>
-            @endif
+            <p>Adicionar Produto</p>
         </div>
         <div class="menu">
             <ul>
@@ -19,33 +15,8 @@
         </div>
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                @if (isset($produto->id))
-                    <form action="{{ route('produto.update', ['produto' => $produto->id]) }}" method="post">
-                        @csrf
-                        @method('PUT')
-                @else
-                    <form action="{{ route('produto.store') }}" method="post">
-                        @csrf
-                @endif
-                        <input type="text" value="{{ $produto->nome ?? old('nome') }}" name="nome" placeholder="Nome" class="borda-preta">
-                        {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-
-                        <input type="text" value="{{ $produto->descricao ?? old('descricao') }}" name="descricao" placeholder="Descrição" class="borda-preta">
-                        {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-
-                        <input type="text" value="{{ $produto->peso ?? old('peso') }}" name="peso" placeholder="Peso" class="borda-preta">
-                        {{ $errors->has('peso') ? $errors->first('peso') : '' }}
-
-                        <select name="unidade_id" placeholder="E-mail" class="borda-preta">
-                            <option>-- Selecione a unidade de medida --</option>
-                            @foreach ($unidades as $unidade)
-                                <option value="{{ $produto->unidade_id ?? $unidade->id }}" {{ old('unidade_id') == $unidade->id ? 'selected' : '' }}>{{ $unidade->descricao }}</option>
-                            @endforeach
-                        </select>
-                        {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
-
-                        <button type="submit" class="borda-preta">Cadastrar</button>
-                    </form>
+                @component('app.produto._components.form_create_edit', ['unidades' => $unidades])
+                @endcomponent
             </div>
         </div>
     </div>
--- PHP/app_super_gestao/resources/views/app/produto/edit.blade.php
+++ PHP/app_super_gestao/resources/views/app/produto/edit.blade.php
@@ -15,28 +15,8 @@
         </div>
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form action="{{ route('produto.update', ['produto' => $produto->id]) }}" method="post">
-                    @csrf
-                    @method('PUT')
-                    <input type="text" value="{{ $produto->nome ?? old('nome') }}" name="nome" placeholder="Nome" class="borda-preta">
-                    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-
-                    <input type="text" value="{{ $produto->descricao ?? old('descricao') }}" name="descricao" placeholder="Descrição" class="borda-preta">
-                    {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-
-                    <input type="text" value="{{ $produto->peso ?? old('peso') }}" name="peso" placeholder="Peso" class="borda-preta">
-                    {{ $errors->has('peso') ? $errors->first('peso') : '' }}
-
-                    <select name="unidade_id" placeholder="E-mail" class="borda-preta">
-                        <option>-- Selecione a unidade de medida --</option>
-                        @foreach ($unidades as $unidade)
-                            <option value="{{ $unidade->id }}" {{ ($produto->unidade_id ?? old('unidade_id')) == $unidade->id ? 'selected' : '' }}>{{ $unidade->descricao }}</option>
-                        @endforeach
-                    </select>
-                    {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
-
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                </form>
+                @component('app.produto._components.form_create_edit', ['produto' => $produto, 'unidades' => $unidades])
+                @endcomponent
             </div>
         </div>
     </div>
