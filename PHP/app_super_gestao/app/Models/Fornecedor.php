ab727759ee69b3dfda6b1df4350381105ab75940
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-08-11 15:23:57 -0300

    Update: Estabelecendo relação 1 para N (hasMany)

--- PHP/app_super_gestao/app/Models/Fornecedor.php
+++ PHP/app_super_gestao/app/Models/Fornecedor.php
@@ -10,7 +10,13 @@ class Fornecedor extends Model
 {
     use HasFactory;
     use SoftDeletes;
-    
+
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
+
+    public function produtos()
+    {
+        // return $this->hasMany('App\Model\Item');
+        return $this->hasMany('App\Models\Item', 'fornecedor_id', 'id');
+    }
 }
