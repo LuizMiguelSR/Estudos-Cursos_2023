d13796c5c6ef147bcdde193a432f7277bdfccdfa
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-08-25 16:35:10 -0300

    Update: Implementando a tela de cadastro de pedidos

--- PHP/app_super_gestao/resources/views/app/pedido/index.blade.php
+++ PHP/app_super_gestao/resources/views/app/pedido/index.blade.php
@@ -22,12 +22,13 @@
                             <th>Cliente</th>
                             <th></th>
                             <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($pedidos as $pedido)
                             <tr>
-                                <td>{{ $pedido->nome }}</td>
+                                <td>{{ $pedido->id }}</td>
                                 <td>{{ $pedido->cliente_id }}</td>
                                 <td><a href="{{ route('pedido.show', ['pedido' => $pedido->id]) }}">Visualizar</td>
                                 <td>
