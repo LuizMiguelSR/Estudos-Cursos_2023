844f14e8b18933aef4317ca5a0e883a67c0a7e96
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-08-29 17:17:18 -0300

    Update: Implementando o relacionamento belongsToMany

--- PHP/app_super_gestao/app/Models/Pedido.php
+++ PHP/app_super_gestao/app/Models/Pedido.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido extends Model
 {
     use HasFactory;
+
+    public function produtos()
+    {
+        //return $this->belongsToMany('App\Models\Produto', 'pedidos_produtos');
+        return $this->belongsToMany('App\Models\Item', 'pedidos_produtos', 'pedido_id', 'produto_id');
+        /*
+            1 - Modelo do relacionamento NxN em relação o modelo que estamos implementando
+            2 - É a tabela auxiliar que armazena os registros de relacionamento
+            3 - Representa o nome da fk da tabela mapeada pelo modelo na tabela de relacionamento
+            4 - Representa o nome da fk da tabela mapeada pelo modelo utilizado no relacionamento
+        */
+    }
 }
