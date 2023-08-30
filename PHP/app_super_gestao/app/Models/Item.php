8ec3566ec7e9816724dad675f1d98c76e86bb44a
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-08-30 15:33:02 -0300

    Update: Praticando um pouco mais o belongsToMany

--- PHP/app_super_gestao/app/Models/Item.php
+++ PHP/app_super_gestao/app/Models/Item.php
@@ -21,4 +21,13 @@ class Item extends Model
     {
         return $this->belongsTo('App\Models\Fornecedor');
     }
+
+    public function pedidos()
+    {
+        return $this->belongsToMany('App\Models\Pedido', 'pedidos_produtos', 'produto_id', 'pedido_id');
+        /*
+            3 - Representa o nome da fk da tabela mapeada pelo model na tabela de relacionamento
+            4 - Representa o nome da fk da tabela mapeada pelo model utilizado no relacionamento que estamos implementando
+        */
+    }
 }
